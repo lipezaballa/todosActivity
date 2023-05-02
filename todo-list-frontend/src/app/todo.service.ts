10fe2ea2c77f4928e6648cfe2a66e114a986accc
lipezaballa <lipezaballa@gmail.com>
2023-05-02 18:07:02 +0200

    suggesting how the API call would be made

--- todo-list-frontend/src/app/todo.service.ts
+++ todo-list-frontend/src/app/todo.service.ts
@@ -1,44 +1,60 @@
-import {Injectable} from '@angular/core';
-import {Observable, of} from "rxjs";
-import {delay, map} from "rxjs/operators";
+import { Injectable } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { delay, map } from "rxjs/operators";
 
 export interface Todo {
-  id: number;
-  task: string;
-  priority: 1 | 2 | 3;
+    id: number;
+    task: string;
+    priority: 1 | 2 | 3;
 }
 
 let mockData: Todo[] = [
-  { id: 0, task: 'Implement loading - frontend only', priority: 1 },
-  { id: 1, task: 'Implement search - frontend only', priority: 2 },
-  { id: 2, task: 'Implement delete on click - frontend only', priority: 1 },
-  { id: 3, task: 'Replace mock service by integrating backend', priority: 3 },
+    { id: 0, task: "Implement loading - frontend only", priority: 1 },
+    { id: 1, task: "Implement search - frontend only", priority: 2 },
+    { id: 2, task: "Implement delete on click - frontend only", priority: 1 },
+    { id: 3, task: "Replace mock service by integrating backend", priority: 3 },
 ];
 
 function removeFromMockData(id: number) {
-  mockData = mockData.filter(todo => todo.id !== id);
+    mockData = mockData.filter((todo) => todo.id !== id);
 }
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: "root",
 })
 export class TodoService {
+    getAll(): Observable<Todo[]> {
+        return of(undefined).pipe(
+            delay(2_000),
+            map(() => mockData)
+        );
+    }
 
-  getAll(): Observable<Todo[]> {
-    return of(undefined).pipe(delay(2_000), map(() => mockData));
-  }
+    // getAllWithFetch(): Observable<Todo[]> {
+    //     let todos: Observable<Todo[]>;
+    //     fetch("http://localhost:8099/api")
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             todos = data.map((todoJson) => JSON.parse(todoJson));
+    //         })
+    //         .catch((error) => console.error(error));
 
-  remove(id: number): Observable<void> {
-    return new Observable<void>(observer => {
-      setTimeout(() => {
-        if (Math.random() < .8) {
-          removeFromMockData(id);
-          observer.next();
-        } else {
-          observer.error('Failed');
-        }
-        observer.complete();
-      }, 2_000)
-    })
-  }
+    //     return todos;
+    // }
+
+    remove(id: number): Observable<void> {
+        return new Observable<void>((observer) => {
+            setTimeout(() => {
+                if (Math.random() < 0.8) {
+                    removeFromMockData(id);
+                    observer.next();
+                } else {
+                    observer.error("Failed");
+                }
+                observer.complete();
+            }, 2_000);
+        });
+    }
+
+    // removefetch(id: number) {} not time enough
 }
